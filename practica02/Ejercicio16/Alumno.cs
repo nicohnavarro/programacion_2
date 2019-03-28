91fa5248debe6f4bbc9443260a96ca809e9a7953
nicohnavarro <nicohnavarro@gmail.com>
2019-03-27 22:02:19 -0300

    Finish exercice 16

--- practica02/Ejercicio16/Alumno.cs
+++ practica02/Ejercicio16/Alumno.cs
@@ -8,5 +8,52 @@ namespace Ejercicio16
 {
     class Alumno
     {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+
+        public  void CalcularFinal()
+        {
+            if(this.nota1>4 && this.nota2>4)
+            {
+                Random nota = new Random();
+                this.notaFinal = nota.Next(1, 10);
+            }
+            else
+            {
+                this.notaFinal = -1;
+            }
+
+        }
+
+        public void Estudiar(byte nota1, byte nota2)
+        {
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+
+        public string Mostrar()
+        {
+            string retorno = "";
+            retorno = "Nombre :" + this.nombre + "\nApellido:" + this.apellido + 
+                "\nLegajo: " + this.legajo + "\nNota 1: "+ this.nota1+"\nNota 2: "+this.nota2;
+
+            if(this.notaFinal==-1)
+            {
+                retorno += "\nNota Final: Alumno Desaporbado \n\n";
+            }
+            else
+            {
+                retorno += "\nNota Final:"+ this.notaFinal+ "\n\n";
+            }
+            return retorno;
+        }
+
+
     }
 }
--- practica02/Ejercicio16/Program.cs
+++ practica02/Ejercicio16/Program.cs
@@ -10,6 +10,37 @@ namespace Ejercicio16
     {
         static void Main(string[] args)
         {
+            Alumno alumno1;
+            Alumno alumno2;
+            Alumno alumno3;
+
+
+            alumno1 = new Alumno();        
+            alumno1.apellido = "Perez";
+            alumno1.legajo = 1;
+            alumno1.nombre = "Juan";
+
+            alumno2 = new Alumno();
+            alumno2.apellido = "Rodriguez";
+            alumno2.legajo = 2;
+            alumno2.nombre = "Pedro";
+
+            alumno3 = new Alumno();
+            alumno3.apellido = "Gonzalez";
+            alumno3.legajo = 3;
+            alumno3.nombre = "Martin";
+
+            alumno1.Estudiar(10, 9);
+            alumno2.Estudiar(6, 6);
+            alumno3.Estudiar(7, 2);
+
+            alumno1.CalcularFinal();
+            alumno2.CalcularFinal();
+            alumno3.CalcularFinal();
+
+            Console.WriteLine(alumno1.Mostrar() + alumno2.Mostrar() + alumno3.Mostrar());
+
+            Console.ReadKey();
         }
     }
 }
