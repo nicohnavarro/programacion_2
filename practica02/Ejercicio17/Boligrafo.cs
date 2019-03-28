e9460ac9eaec1486b313d03aad35697ea4b666e7
nicohnavarro <nicohnavarro@gmail.com>
2019-03-28 02:51:26 -0300

    Finish Exercice 17

---
+++ practica02/Ejercicio17/Boligrafo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+    class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            short tintaBoligrafo = GetTinta();
+            if(tinta<0)
+            {
+                this.tinta += tinta;
+                if(this.tinta<0)
+                {
+                    this.tinta = 0;
+                }
+            }
+            if(tinta>0 && (tintaBoligrafo+tinta)<=cantidadTintaMaxima)
+            {
+                this.tinta += tinta;
+            }
+        }
+
+        public void Recargar()
+        {
+            short tintaARecargar;
+            short tintaBoligrafo = GetTinta();
+
+            tintaARecargar =(short)(cantidadTintaMaxima - tintaBoligrafo);
+
+            SetTinta(tintaARecargar);
+        }
+
+        public bool Pintar(int gasto,out string dibujo)
+        {
+            bool retorno = true;
+            short gastoTinta;
+            short tintaBoligrafo=GetTinta();
+            short cantidadSobrante;
+            gastoTinta = (short)(gasto - (gasto * 2));
+            cantidadSobrante = (short)(gasto - tintaBoligrafo);
+            dibujo = "";
+            if (tintaBoligrafo>0)
+            {
+                SetTinta(gastoTinta);
+                
+                for (int i = 0; i < gasto; i++)
+                {
+                    dibujo += "*";
+                }
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ practica02/Ejercicio17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo boligrafo1 = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo boligrafo2 = new Boligrafo(50, ConsoleColor.Red);
+            string pintura;
+
+            Console.ForegroundColor = boligrafo1.GetColor();
+            boligrafo1.Pintar(50,out pintura);
+            Console.WriteLine(pintura);
+
+            Console.ForegroundColor = boligrafo2.GetColor();
+            boligrafo2.Pintar(50,out pintura);
+            Console.WriteLine(pintura);
+
+            Console.ForegroundColor =ConsoleColor.White;
+            Console.WriteLine("Tinta Boligrafo 1 :{0}", boligrafo1.GetTinta());
+            Console.WriteLine("Tinta Boligrafo 2 :{0}", boligrafo2.GetTinta());
+            boligrafo2.Recargar();
+            Console.WriteLine("RECARGA BOLIGRAFO 2 ...");
+            Console.WriteLine("Tinta Boligrafo 1 :{0}", boligrafo1.GetTinta());
+            Console.WriteLine("Tinta Boligrafo 2 :{0}", boligrafo2.GetTinta());
+
+            Console.ReadKey();
+
+        }
+    }
+}
