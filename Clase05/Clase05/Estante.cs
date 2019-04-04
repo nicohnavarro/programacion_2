e0973d25f360b543b2d65f74aca8fc45952d4e03
nicohnavarro <nicohnavarro@gmail.com>
2019-04-03 21:31:45 -0300

    Exercice Class 5

---
+++ Clase05/Clase05/Estante.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase05
+{
+    class Estante
+    {
+        Producto[] _productos;
+        int _ubicacionEstante;
+
+        /// <summary>
+        /// Private Builder
+        /// </summary>
+        /// <param name="capacidad">capacidad</param>
+        private Estante(int capacidad)
+        {
+            this._productos = new Producto[capacidad];
+        }
+
+        /// <summary>
+        /// Builder with parametres
+        /// </summary>
+        /// <param name="capacidad">int capacidad</param>
+        /// <param name="ubicacion">int ubicacion</param>
+        public Estante(int capacidad,int ubicacion):this(capacidad)
+        {
+            this._ubicacionEstante = ubicacion;
+            
+        }
+
+        /// <summary>
+        /// Getter of product
+        /// </summary>
+        /// <returns>Array of product</returns>
+        public Producto[] GetProductos()
+        {
+            return this._productos;
+        }
+
+        /// <summary>
+        /// Show Estante with all product
+        /// </summary>
+        /// <param name="e">estante</param>
+        /// <returns>string with all information</returns>
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder infoEstante = new StringBuilder();
+            string returnInfo;
+            infoEstante.AppendFormat("Estante {0}", e._ubicacionEstante);
+            foreach(Producto producto in e.GetProductos())
+            {
+                if(!(Object.ReferenceEquals(producto,null)))
+                {
+                    infoEstante.AppendLine(Producto.MostrarProducto(producto));
+                }
+            }
+            returnInfo = infoEstante.ToString();
+            return returnInfo;
+        }
+
+        /// <summary>
+        /// Overload of operator ==
+        /// </summary>
+        /// <param name="e">estante</param>
+        /// <param name="p">producto</param>
+        /// <returns>true if e contains p or false if not or is null</returns>
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool retorno = false;
+            foreach (Producto producto in e.GetProductos())
+            {
+                if (!(Object.ReferenceEquals(producto, null)) && producto == p)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Overload of operator !=
+        /// </summary>
+        /// <param name="e">estante</param>
+        /// <param name="p">producto</param>
+        /// <returns>true if e not include p or false if e contains p</returns>
+        public static bool operator !=(Estante e, Producto p)
+        {
+            bool retorno = !(e == p);
+            return retorno;
+        }
+
+
+        public static bool operator +(Estante e,Producto p)
+        {
+            bool retorno = false;
+            int cantidad=0;
+            foreach (Producto producto in e.GetProductos())
+            {
+                if (!(Object.ReferenceEquals(producto, null)))
+                {
+                    cantidad++;
+                    Console.WriteLine(cantidad);
+                }
+            }
+            if(cantidad>0 && cantidad<e.GetProductos().Length)
+            {
+                retorno = true;
+                e.GetProductos()[cantidad] = p;
+            }
+            else if(cantidad==e.GetProductos().Length)
+            {
+                retorno = false;
+            }
+            return retorno;
+        }
+
+        
+    }
+}
---
+++ Clase05/Clase05/Producto.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase05
+{
+    class Producto
+    {
+        string _codigoDeBarra;
+        string _marca;
+        float _precio;
+
+        /// <summary>
+        /// Builder with parametres
+        /// </summary>
+        /// <param name="marca">marca of product</param>
+        /// <param name="codigo">code of product</param>
+        /// <param name="precio">price of product</param>
+        public Producto(string marca,string codigo,float precio)
+        {
+            this._marca = marca;
+            this._precio = precio;
+            this._codigoDeBarra = codigo;
+        }
+
+        /// <summary>
+        /// Overload explicit of operator string
+        /// </summary>
+        /// <param name="p"></param>
+        public static explicit operator string(Producto p)
+        {
+            string retorno;
+            retorno = (p._codigoDeBarra).ToString();
+            return retorno;
+        }
+
+        /// <summary>
+        /// Getter of marca
+        /// </summary>
+        /// <returns>marca of product</returns>
+        public string GetMarca()
+        {
+            return this._marca;
+        }
+
+        /// <summary>
+        /// Getter of price
+        /// </summary>
+        /// <returns>price of producto</returns>
+        public float GetPrecio()
+        {
+            return this._precio;
+        }
+        
+        /// <summary>
+        /// Show the Product with attributes
+        /// </summary>
+        /// <param name="p">object Producto</param>
+        /// <returns>producto with marca,price and code</returns>
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder infoProducto = new StringBuilder();
+            string showProduct;
+
+            infoProducto.AppendFormat("Producto : \nMarca:{0}\nPrecio: ${1}\nCodigo de Barra: {2}\n", p.GetMarca(), p.GetPrecio(), p._codigoDeBarra);
+            showProduct = infoProducto.ToString();
+            return showProduct;
+        }
+
+        /// <summary>
+        /// Overload of operator !=
+        /// </summary>
+        /// <param name="p">product</param>
+        /// <param name="marca">string marca</param>
+        /// <returns>true if they are != or false if not</returns>
+        public static bool operator !=(Producto p,string marca)
+        {
+            bool retorno = false;
+            if(p.GetMarca()!=marca)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Overload of operator ==
+        /// </summary>
+        /// <param name="p">product</param>
+        /// <param name="marca">string marca</param>
+        /// <returns>true if they are == or false if not</returns>
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool retorno = false;
+            if (p.GetMarca() == marca)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Overload of operator !=
+        /// </summary>
+        /// <param name="p1">Product one</param>
+        /// <param name="p2">Product two</param>
+        /// <returns>true if they are != or false if not</returns>
+        public static bool operator !=(Producto p1,Producto p2)
+        {
+            bool retorno = false;
+            if(p1.GetMarca()!=p2.GetMarca() && p1._codigoDeBarra!=p2._codigoDeBarra)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Overload of operator ==
+        /// </summary>
+        /// <param name="p1">Product one</param>
+        /// <param name="p2">Product two</param>
+        /// <returns>true if they are == or false if not</returns>
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+            if (p1.GetMarca() == p2.GetMarca() && (string)p1 == (string)p2)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+    }
+}
