e1dde70ef33491ad1934fababfb1237921f4be1f
nicohnavarro <nicohnavarro@gmail.com>
2019-04-11 20:19:04 -0300

    Exercice 29 Players and Teams

---
+++ Ejercicio29/Ejercicio29/Equipo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio29
+{
+    class Equipo
+    {
+
+        private short cantidadDeJugadores;
+        private List<Jugador> jugadores;
+        private string nombre;
+
+        private Equipo()
+        {
+            this.jugadores = new List<Jugador>();
+        }
+
+        public Equipo(short cantidad, string nombre) : this()
+        {
+            this.cantidadDeJugadores = cantidad;
+            this.nombre = nombre;
+        }
+
+        public static bool operator +(Equipo equipo, Jugador jugador1)
+        {
+            bool retorno = false;
+            if(equipo.jugadores.Count()==0)
+            {
+                equipo.jugadores.Add(jugador1);
+                retorno = true;
+            }
+            else
+            {
+                foreach (Jugador jugador2 in equipo.jugadores)
+                {
+                    if (jugador1 != jugador2 && equipo.jugadores.Count() < equipo.cantidadDeJugadores)
+                    {
+                        equipo.jugadores.Add(jugador1);
+                        retorno = true;
+                    }
+                    else
+                    {
+                        retorno = false;
+                    }
+                }
+            }
+            return retorno;
+        }
+    }
+}
---
+++ Ejercicio29/Ejercicio29/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equipo equipo1=new Equipo(11,"Barcelona");
+            Equipo equipo2 = new Equipo(11, "Juventus");
+
+            Jugador jugador1 = new Jugador(35462712,"Messi",20,10);
+            Jugador jugador2 = new Jugador(35462712, "Mezzi", 30, 10);
+            Jugador jugador3 = new Jugador(32262712, "Cristiano", 40, 10);
+
+            if(equipo1+jugador1)
+            {
+                Console.WriteLine("Se Agrego a Leo");
+            }
+            if(!(equipo1+jugador2))
+            {
+                Console.WriteLine("no Se Agrego a Leo2");
+            }
+
+            Console.WriteLine(jugador1.MostrarDatos());
+            Console.WriteLine(jugador3.MostrarDatos());
+            Console.ReadKey();
+
+
+        }
+    }
+}
